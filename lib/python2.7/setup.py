00ed9957f31a56ef0f4258974ee9f5cac31d1544
you <you>
2012-02-05 21:39:18 +0900

    kltk python: setup

---
+++ lib/python2.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kltk',
+    version = '0.1',
+    packages = find_packages(),
+    author = ['youhyunjo'],
+    author_email = ['youhyunjo@gmail.com'],
+    url = 'http://kltk.googlecode.com',
+    description = 'Korean Language Toolkit',
+    license = "GNU GPL3",
+    )
